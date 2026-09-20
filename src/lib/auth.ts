7927a437e6583219887ae8ed17c592c72608a708
בן ברעם <benbaramqa@gmail.com>
2026-09-20 11:04:27 +0300

    Remove unconfigured Google Auth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
 import { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '@/lib/prisma';
@@ -7,10 +6,6 @@ import prisma from '@/lib/prisma';
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
